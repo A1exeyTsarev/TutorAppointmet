480e102f7b69238bb18c6f8c953582d186da7706
A1exeyTsarev <a.d.tsarev@mail.ru>
2026-03-30 14:05:24 +0300

    1

--- TutorAppointment_New/EditLesson.Designer.cs
+++ TutorAppointment_New/EditLesson.Designer.cs
@@ -153,6 +153,7 @@
             //this.Notes.TextChanged += new System.EventHandler(this.Notes_TextChanged);
             // 
             // label6
+
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
